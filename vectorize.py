66e410deaa8610dfa0b8e572072ddf1b4ac9b119
lukaszkuczynski <kuczynskilukasz@gmail.com>
2019-02-11 17:56:05 +0100

    process simplified, get rid of custom tokenization and cleaning

--- vectorize.py
+++ vectorize.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from nltk.tokenize import RegexpTokenizer
 import pickle
 from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def df_from_files(path, file_tags):
@@ -15,32 +15,6 @@ def df_from_files(path, file_tags):
     return df
 
 
-def tokenize(df):
-    tokenizer = RegexpTokenizer(r'\w+')
-
-    def tokenize_row(row):
-        tokens = tokenizer.tokenize(row['content'])
-        row['tokens'] = [str.lower(token) for token in tokens]
-        return row
-
-    df = df.apply(tokenize_row, axis=1)
-    return df
-
-
-def clean(df):
-    import nltk
-    from nltk.corpus import stopwords
-    nltk.download('stopwords')
-
-    def remove_stopwords(row):
-        stop_words = stopwords.words('english')
-        row['cleaned'] = [w for w in row['tokens'] if not w in stop_words] 
-        return row
-    
-    df = df.apply(remove_stopwords, axis=1)
-    return df
-
-
 def label(df):
     label_encoder = LabelEncoder()
     labels = label_encoder.fit_transform(df['tag_text'])
@@ -49,11 +23,9 @@ def label(df):
 
 
 def fit_vectorizer(df):
-    from sklearn.feature_extraction.text import CountVectorizer, HashingVectorizer, TfidfVectorizer
     vectorizer = TfidfVectorizer(max_df=0.5, min_df=4, use_idf=True)
-    df['features'] = df.apply(lambda row: ' '.join(row['cleaned']), axis=1)
-    vectorizer = vectorizer.fit(df['features'])
-    X = vectorizer.transform(df['features'])
+    vectorizer = vectorizer.fit(df['content'])
+    X = vectorizer.transform(df['content'])
     return vectorizer, X
 
 
@@ -80,13 +52,11 @@ if __name__ == "__main__":
         'devops' : 'devops_posts.csv',
     }
     df_read = df_from_files(data_path, file_tags)
-    df_tokenized = tokenize(df_read)
-    df_cleaned = clean(df_tokenized)
-    df_labeled, label_encoder = label(df_cleaned)
+    df_labeled, label_encoder = label(df_read)
     vectorizer, X = fit_vectorizer(df_labeled)
     vectorizer_path = save_vectorizer(save_path, vectorizer)
     save_pickle(save_path+'X.pickle', X)
-    save_pickle(save_path+'y.pickle', df_cleaned['label'])
+    save_pickle(save_path+'y.pickle', df_read['label'])
     save_pickle(save_path+'label_encoder.pickle', label_encoder)
     print("Data fitted, vectorizer saved as file to path '%s'" % vectorizer_path)
     print("X and y saved as file to path '%s'" % save_path)

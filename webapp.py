3df19bc677ca09ef5f2cfb64f4f1dd3c2d73b6cc
lukaszkuczynski <kuczynskilukasz@gmail.com>
2019-02-11 18:18:33 +0100

    Prediction visible in a Flask app

--- webapp.py
+++ webapp.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 import datetime, logging
 from logging import handlers
+from predict import Predictor, from_pickle
+
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -18,13 +20,21 @@ handler = handlers.RotatingFileHandler(
 
 app.logger.addHandler(handler)
 
+model = from_pickle('bayes.pickle')
+vectorizer = from_pickle('vectorizer.pickle')
+label_encoder = from_pickle('label_encoder.pickle')
+predictor = Predictor(model, vectorizer, label_encoder)
+
+
+
 @app.route("/", methods=['GET','POST'])
 def hello():
     if request.method == 'GET':
         message = "This is front page"
     else:
         content = request.form['content']
-        message = 'WAS POSTED'
+        prediction = predictor.predict(content)
+        message = str(prediction)
     return render_template("index.html", message = message)
 
 
